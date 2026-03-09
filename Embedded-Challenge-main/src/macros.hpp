e9afe849aa91facb49d020fe61c5ca2d899f4fc0
Neha Patil <76066600+patilneha08@users.noreply.github.com>
2026-03-09 16:25:24 -0400

    Delete Embedded-Challenge-main directory

--- Embedded-Challenge-main/src/macros.hpp
+++
@@ -1,7 +0,0 @@
-#ifndef MACROS_H
-#define MACROS_H
-
-// Maximum size for arrays used in the program (e.g., for storing sensor data or gesture points)
-#define MAX_ARRAY_SIZE 50
-
-#endif // MACROS_H
